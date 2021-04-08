2648393290e4f95b4165e54888a55c3bc40aa4d8
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-04-08 13:39:59 +0200

    small changes

--- audio_utils/mel/msd.py
+++ audio_utils/mel/msd.py
@@ -5,15 +5,16 @@ from fastdtw.fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 
 
-def align_mel_spectograms_with_dtw(mel_spec_1: np.ndarray, mel_spec_2: np.ndarray) -> Tuple[np.ndarray, np.ndarray, float]:
+def align_mels_with_dtw(mel_spec_1: np.ndarray, mel_spec_2: np.ndarray) -> Tuple[np.ndarray, np.ndarray, float]:
   mel_spec_1, mel_spec_2 = mel_spec_1.T, mel_spec_2.T
   dist, path = fastdtw(mel_spec_1, mel_spec_2, dist=euclidean)
   path_for_mel_spec_1 = list(map(lambda l: l[0], path))
   path_for_mel_spec_2 = list(map(lambda l: l[1], path))
   aligned_mel_spec_1 = mel_spec_1[path_for_mel_spec_1]
   aligned_mel_spec_2 = mel_spec_2[path_for_mel_spec_2]
-  final_frame_number = len(path)
-  msd = dist / final_frame_number
-  assert final_frame_number == aligned_mel_spec_1.shape[1]
-  assert final_frame_number == aligned_mel_spec_2.shape[1]
-  return aligned_mel_spec_1, aligned_mel_spec_2, msd
+  return aligned_mel_spec_1, aligned_mel_spec_2, dist
+
+
+def get_msd(dist: float, total_frame_number: int) -> float:
+  msd = dist / total_frame_number
+  return msd
