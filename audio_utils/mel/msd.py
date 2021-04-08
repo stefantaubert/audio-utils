d9f8ec8cff8d36cca0c71d988da4eb68e7e0af19
Stefan Taubert <stefan.taubert@posteo.de>
2021-04-08 14:31:02 +0200

    bugfix mel dtw

--- audio_utils/mel/msd.py
+++ audio_utils/mel/msd.py
@@ -12,7 +12,7 @@ def align_mels_with_dtw(mel_spec_1: np.ndarray, mel_spec_2: np.ndarray) -> Tuple
   path_for_mel_spec_2 = list(map(lambda l: l[1], path))
   aligned_mel_spec_1 = mel_spec_1[path_for_mel_spec_1]
   aligned_mel_spec_2 = mel_spec_2[path_for_mel_spec_2]
-  return aligned_mel_spec_1, aligned_mel_spec_2, dist
+  return aligned_mel_spec_1.T, aligned_mel_spec_2.T, dist
 
 
 def get_msd(dist: float, total_frame_number: int) -> float:
